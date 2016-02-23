fd7317afb056a7c850b2c8ef11f5bd08c721357e
Ken Hwang <krh5058@psu.edu>
2016-02-23 09:45:38 -0500

    Mapped no auth route to not include controller
    160223
    0945
    krh
    
    - /Home/NotAuthorized > /NotAuthorized

--- MvcBoilerPlate/App_Start/RouteConfig.cs
+++ MvcBoilerPlate/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MvcBoilerPlate
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "NoAuth",
+                url: "NotAuthorized/",
+                defaults: new { controller = "Home", action = "NotAuthorized", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
