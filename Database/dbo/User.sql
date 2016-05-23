9672b34a4072764078dc1cf51bbb16c8e5cb53d5
kennyhwang <ken.r.hwang@gmail.com>
2016-05-23 15:46:51 -0400

    Added UnitTest Project

--- Database/dbo/User.sql
+++ Database/dbo/User.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[UserId] INT NOT NULL PRIMARY KEY, 
     [FirstName] NCHAR(10) NOT NULL, 
     [LastName] NCHAR(10) NOT NULL
 )
---
+++ UnitTest/HomeControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using MvcBoilerPlate.Controllers;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [Test]
+        public void TestView_Index_ShouldCreate()
+        {
+            var controller = new HomeController();
+            var result = (ViewResult)controller.Index();
+            Assert.IsNotNull(result);
+        }
+    }
+}
